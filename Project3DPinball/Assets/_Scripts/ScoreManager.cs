93b244a68f994146fc2ca0318c5501c64065ec48
Sebastiaan <Buwalda@MacBook-Pro-van-Buwalda.local>
2015-09-28 10:53:32 +0200

    Added _Scripts

---
+++ Project3DPinball/Assets/_Scripts/ScoreManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public int myScore = 0;
+
+public class ScoreManager : MonoBehaviour {
+	
+	void Start () {
+		Debug.Log(myScore);
+	}
+
+	void Update () {
+	
+	}
+}
