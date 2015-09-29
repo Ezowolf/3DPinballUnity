0ccf1b1cd75fcbd7fed13ee8465e7fd2a72cf131
Sebastiaan <Buwalda@mbp-van-buwalda.media.local>
2015-09-29 11:51:58 +0200

    Score UI + Score Updating

---
+++ Project3DPinball/Assets/_Scripts/ScoreCounter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreCounter : MonoBehaviour {
+
+	private int myScore = 0;
+	public ScoreUIUpdater myUIUpdater;
+
+	void AddExtraScore(){
+		myScore = myScore + 100;
+		myUIUpdater.updateTheUI(myScore);
+	}
+
+	void Update(){
+		AddExtraScore();
+	}
+}
---
+++ Project3DPinball/Assets/_Scripts/ScoreUIUpdater.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreUIUpdater : MonoBehaviour {
+	public Text textToDisplayScore;
+		
+	public void updateTheUI(int newScore)
+	{
+		textToDisplayScore.text = "Score: "+newScore;
+	}
+}
