939176f640add5d90bb62c239af2cc146a765914
Sebastiaan <Buwalda@MacBook-Pro-van-Buwalda.local>
2015-09-29 21:01:34 +0200

    Sounds+menu

--- Project3DPinball/Assets/_Scripts/ScoreCounter.cs
+++ Project3DPinball/Assets/_Scripts/ScoreCounter.cs
@@ -5,9 +5,11 @@ public class ScoreCounter : MonoBehaviour {
 
 	private int myScore = 0;
 	public ScoreUIUpdater myUIUpdater;
+	public AudioSource soundWhenExtraScore;
 
 	void AddExtraScore(){
 		myScore = myScore + 100;
+		soundWhenExtraScore.Play();
 		myUIUpdater.updateTheUI(myScore);
 	}
 
