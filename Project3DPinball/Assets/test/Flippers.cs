4e90b22b5070604b35d601ea0f7570cb14039334
Tim <tbajmat@gmail.com>
2015-10-11 21:49:32 +0200

    Flippers

---
+++ Project3DPinball/Assets/test/Flippers.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flippers : MonoBehaviour {
+	private float rotitions = 0;
+	private bool open = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	/*void OnEnable()
+	{
+		KeyboardInput.DoThings += Pressed ();
+	}
+	
+	void OnDiable()
+	{
+		KeyboardInput.DoThings -= Pressed ();
+	}*/
+	void Pressed()
+	{
+		open = true;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Pressed ();
+		}
+	
+
+		if (open) {
+			if (rotitions < 90) {
+				transform.Rotate (Vector3.up * 10);
+				rotitions += 10;
+
+			} else {
+				open = false;
+			}
+		} 
+		else 
+		{
+			if (rotitions >0) {
+				transform.Rotate (Vector3.up * -10);
+				rotitions -= 10;
+			}
+		}
+	}
+
+}
