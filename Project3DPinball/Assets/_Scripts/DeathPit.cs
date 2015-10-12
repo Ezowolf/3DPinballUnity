7072a06ae6960e250b825de54a337cce8e0b9da9
Sebastiaan <Buwalda@mbp-van-buwalda.media.local>
2015-10-12 10:08:23 +0200

    Death Pit

---
+++ Project3DPinball/Assets/_Scripts/DeathPit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathPit : MonoBehaviour {
+	private bool haveIDestroyedBall = false;
+
+	public AudioSource soundWhenBallDestroys;
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		if(collision.gameObject.tag == "BallTag")
+		{
+			Destroy(collision.gameObject);
+			haveIDestroyedBall = true;
+			soundWhenBallDestroys.Play();
+		}
+	}
+}
