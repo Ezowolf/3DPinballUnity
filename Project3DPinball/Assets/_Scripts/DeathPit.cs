50eeed6ea849817133f7c79a76f9d9121f0db0e9
Sebastiaan <Buwalda@mbp-van-buwalda.media.local>
2015-10-13 09:05:47 +0200

    Game Over!

--- Project3DPinball/Assets/_Scripts/DeathPit.cs
+++ Project3DPinball/Assets/_Scripts/DeathPit.cs
@@ -1,18 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DeathPit : MonoBehaviour {
 	private bool haveIDestroyedBall = false;
 
 	public AudioSource soundWhenBallDestroys;
-
+	public Text gameOverText;
+	
+	void Start()
+	{
+		gameOverText.text = "";
+	}
 	void OnCollisionEnter(Collision collision) 
 	{
 		if(collision.gameObject.tag == "BallTag")
 		{
-			Destroy(collision.gameObject);
-			haveIDestroyedBall = true;
-			soundWhenBallDestroys.Play();
+			GameOver(collision);
 		}
+	}
+
+	void GameOver(Collision objectToDestroy)
+	{
+		//check if you still have balls left?
+		Destroy(objectToDestroy.gameObject);
+		haveIDestroyedBall = true;
+		soundWhenBallDestroys.Play();
+		gameOverText.text = "GAME OVER";
 	}
 }
