86a5668482f50ca345b5bdb4a8042bc7d9fabd0f
Sebastiaan <Buwalda@mbp-van-buwalda.media.local>
2015-10-13 09:42:08 +0200

    Gravity + Prefab Update
    Tim, you can work in the FinalGameScene to fix the flipper.

---
+++ Project3DPinball/Assets/_Scripts/PinballGravity.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinballGravity : MonoBehaviour {
+	public Vector3 myGravity;
+	
+	void Start () {
+		Physics.gravity = myGravity;
+	}
+}
