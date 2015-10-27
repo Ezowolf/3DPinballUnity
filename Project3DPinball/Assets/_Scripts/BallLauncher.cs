a731de3eb29fdb60e2a42c762895c08109981b3b
Sebastiaan <Buwalda@mbp-van-buwalda.media.local>
2015-10-27 09:43:55 +0100

    BallLauncher

---
+++ Project3DPinball/Assets/_Scripts/BallLauncher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallLauncher : MonoBehaviour {
+	public float minSize;
+	public Vector3 startingSize = new Vector3(1,1,1);
+	void Update () {
+	if(Input.GetKey(KeyCode.Space))
+	{
+			if(transform.localScale.z>minSize)
+			{
+				transform.localScale += (new Vector3(0F, 0F, -0.5F)*(Time.deltaTime*100));
+			}
+
+	}
+		else
+		{
+			if(transform.localScale.z<startingSize.z)
+			{
+				transform.localScale -= (new Vector3(0F, 0F, -0.5F)*(Time.deltaTime*100));
+			}
+		}
+
+	}
+}
