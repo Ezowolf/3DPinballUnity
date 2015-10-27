3cd08645051a4cc6d84af264a6874f2b1ed342d6
Sebastiaan <Buwalda@mbp-van-buwalda.media.local>
2015-10-27 11:46:39 +0100

    Better spring, bugfixes

---
+++ Project3DPinball/Assets/_Scripts/AddForceToBall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddForceToBall : MonoBehaviour {
+	private bool canIBeLaunched = true;
+	public float launchForce;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void Update () {
+		if (Input.GetKeyUp("space")&&canIBeLaunched==true)
+		{
+			canIBeLaunched = false;
+			rb.AddForce(transform.forward * launchForce );
+
+		}
+}
+}
---
+++ Project3DPinball/Assets/_Scripts/LimitBallSpeed.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimitBallSpeed : MonoBehaviour {
+	public float maxSpeed = 200f;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate()
+	{
+		Debug.Log(rb.velocity.magnitude);
+		if(rb.velocity.magnitude > maxSpeed)
+		{
+			Debug.Log("LIMIT!");
+			rb.velocity = rb.velocity.normalized * maxSpeed;
+		}
+	}
+}
